d00040d736c527adb051f1d7dbcc39d40fcda59d
ajharul <mdajharulislam327@gmail.com>
2025-12-16 13:13:38 +0600

    done

---
+++ src/constants.js
@@ -0,0 +1,307 @@
+// Skills Section Logo's
+import htmlLogo from './assets/tech_logo/html.png';
+import cssLogo from './assets/tech_logo/css.png';
+import sassLogo from './assets/tech_logo/sass.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import reactjsLogo from './assets/tech_logo/reactjs.png';
+
+import reduxLogo from './assets/tech_logo/redux.png';
+import nextjsLogo from './assets/tech_logo/nextjs.png';
+import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
+import gsapLogo from './assets/tech_logo/gsap.png';
+import materialuiLogo from './assets/tech_logo/materialui.png';
+import bootstrapLogo from './assets/tech_logo/bootstrap.png';
+
+import nodejsLogo from './assets/tech_logo/nodejs.png';
+import rest from './assets/tech_logo/rest.png';
+import socket from './assets/tech_logo/socket.png';
+import jwt from './assets/tech_logo/jwt.png';
+import Oauth from './assets/tech_logo/Oauth.png';
+import expressjsLogo from './assets/tech_logo/express.png';
+import mysqlLogo from './assets/tech_logo/mysql.png';
+import mongodbLogo from './assets/tech_logo/mongodb.png';
+import firebaseLogo from './assets/tech_logo/firebase.png';
+import cLogo from './assets/tech_logo/c.png';
+import cppLogo from './assets/tech_logo/cpp.png';
+
+import typescriptLogo from './assets/tech_logo/typescript.png';
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+import postmanLogo from './assets/tech_logo/postman.png';
+import mcLogo from './assets/tech_logo/mc.png';
+import figmaLogo from './assets/tech_logo/figma.png';
+import netlifyLogo from './assets/tech_logo/netlify.png';
+import vercelLogo from './assets/tech_logo/vercel.png';
+import postgreLogo from './assets/tech_logo/postgre.png';
+
+
+// Experience Section Logo's
+import webverseLogo from './assets/company_logo/webverse_logo.png';
+
+
+// Education Section Logo's
+import northern from './assets/education_logo/northern.jpg';
+import tarail from './assets/education_logo/tarail.jpg';
+
+// Project Section Logo's
+import car from './assets/work_logo/car-rental.png';
+
+import amazon from './assets/work_logo/amazon.jpg';
+import blog from './assets/work_logo/blogstudio-ai.png';
+import chat from './assets/work_logo/chatApp.png';
+import cofee from './assets/work_logo/cofee.jpg';
+import ecom from './assets/work_logo/ecom.png';
+import grocery from './assets/work_logo/grocery.png';
+import journalism from './assets/work_logo/journalism.jpg';
+import location from './assets/work_logo/location_t.png';
+import netflix from './assets/work_logo/netflix.jpg';
+
+
+
+export const SkillsInfo = [
+  {
+    title: 'Frontend',
+    skills: [
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+      { name: 'SASS', logo: sassLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'React JS', logo: reactjsLogo },
+      { name: 'TypeScript', logo: typescriptLogo },
+      { name: 'Redux', logo: reduxLogo },
+      { name: 'Next JS', logo: nextjsLogo },
+      { name: 'Tailwind CSS', logo: tailwindcssLogo },
+      { name: 'GSAP', logo: gsapLogo },
+      { name: 'Material UI', logo: materialuiLogo },
+      { name: 'Bootstrap', logo: bootstrapLogo },
+      
+    ],
+  },
+  {
+    title: 'Backend',
+    skills: [
+    
+      { name: 'Node JS', logo: nodejsLogo },
+      { name: 'Express JS', logo: expressjsLogo },
+      { name: 'MySQL', logo: mysqlLogo },
+      { name: 'MongoDB', logo: mongodbLogo },
+      { name: 'Firebase', logo: firebaseLogo },
+      { name: 'PostgreSQL', logo: postgreLogo },
+      { name: 'Rest API', logo: rest },
+      { name: 'Socket.io', logo: socket },
+      { name: 'Oauth', logo: Oauth },
+      { name: 'JWT', logo: jwt },
+    ],
+  },
+  {
+    title: 'Languages',
+    skills: [
+      { name: 'C', logo: cLogo },
+      { name: 'C++', logo: cppLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'TypeScript', logo: typescriptLogo },
+    ],
+  },
+  {
+    title: 'Tools',
+    skills: [
+      { name: 'Git', logo: gitLogo },
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+      { name: 'Postman', logo: postmanLogo },
+      { name: 'Compass', logo: mcLogo },
+      { name: 'Vercel', logo: vercelLogo },
+      { name: 'Netlify', logo: netlifyLogo },
+      { name: 'Figma', logo: figmaLogo },
+    ],
+  },
+];
+
+  export const experiences = [
+    {
+      id: 0,
+      img: webverseLogo,
+      role: "Fullstack Developer",
+      company: "Freelance",
+      date: "April 2023 - Present",
+      desc: "Developed dynamic and scalable web applications using the MERN stack, handling both frontend and backend development. Collaborated with cross-functional teams to build responsive UI, implement RESTful APIs, and optimize application performance in an agile environment.",
+      skills: [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "React JS",
+        "TypeScript",
+        "Node JS",
+        "Tailwind CSS",
+        "MongoDb",
+        "Redux",
+        " Next Js",
+      ],
+    },
+    // {
+    //   id: 1,
+    //   img: agcLogo,
+    //   role: "Fullstack Engineer",
+    //   company: "Agumentik Group of Companies",
+    //   date: "July 2023 - March 2024",
+    //   desc: "Contributed to innovative projects as a Fullstack Engineer, leading both frontend and backend development using technologies such as HTML, CSS, JavaScript, PHP, SQL, Bootstrap, and ReactJS. Worked closely with the team to deliver responsive, high-performance web applications and improve user experience through seamless integration of various technologies.",
+    //   skills: [
+    //     "ReactJS",
+    //     "Redux",
+    //     "JavaScript",
+    //     "Tailwind CSS",
+    //     "HTML",
+    //     "CSS",
+    //     "SQL",
+    //   ],
+    // },
+    // {
+    //   id: 2,
+    //   img: newtonschoolLogo,
+    //   role: "Frontend Intern",
+    //   company: "Newton School",
+    //   date: "September 2021 - August 2022",
+    //   desc: "Worked as a Frontend Developer Intern, designing and implementing scalable UI components and responsive websites using HTML, CSS, JavaScript, Bootstrap, and Material UI. Collaborated with the design team to translate wireframes and prototypes from Figma into interactive, user-friendly web pages.",
+    //   skills: [
+    //     "HTML",
+    //     "CSS",
+    //     "Javascript",
+    //     "Bootstrap",
+    //     "Figma",
+    //     "Material UI",
+    //   ],
+    // },
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: northern,
+      school: "Northern University Bangladesh",
+      date: "Sept 2023 - current",
+      grade: "3.70 CGPA",
+      desc: "I have completed my BSc in Computer Science and Engineering from Northern University Bangladesh. During my time at NUB, I gained a strong foundation in programming, software development, and computer science principles. I have studied courses such as Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Web Development, and Software Engineering. I actively participated in various workshops and technical events, which enhanced my skills and knowledge. My experience at Northern University has been instrumental in shaping my technical abilities and professional growth.",
+      degree: "BSc in Computer Science and Engineering",
+    },
+    {
+      id: 2,
+      img: tarail,
+      school: "Tarail muktijoddha govt college",
+      date: "Sept 2020 - Dece 2022",
+      grade: "4.50 GPA",
+      desc: "I have completed my Collage Degree in Science Tarail muktijoddha govt college. During my College, I gained a strong foundation in programming, software development, and computer science principles. I have studied courses such as Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Web Development, and Software Engineering. I actively participated in various workshops and technical events, which enhanced my skills and knowledge. My experience at college has been instrumental in shaping my technical abilities and professional growth.",
+      degree: "HSC in Science",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 0,
+      title: "Motion Rentals",
+      description:
+        "Developed a full-stack car rental application with user authentication, real-time vehicle availability, booking system, secure payment integration, and an admin dashboard to manage cars, users, and bookings efficiently.",
+        image: car,
+      tags: ["JavaScript", "React JS","Node JS", "Express JS", "MongoDb","JWT", "Rest API","Tailwind CSS","Motion"],
+      github: "https://github.com/MdAjharulIslam/Motion-Rentals",
+      webapp: "https://car-rental-rho-umber.vercel.app",
+    },
+    {
+      id: 1,
+      title: "EcoMart",
+      description:
+        "A modern grocery e-commerce platform with a product catalog, user authentication, cart management, order tracking, and integrated online payments. Built using the MERN stack, it ensures a seamless shopping experience.",
+        image: grocery,
+      tags: ["React JS", "Node.js", "MongoDB", "Express", "HTML", "CSS","Motion", "JavaScript"],
+      github: "https://github.com/MdAjharulIslam/EcoMart",
+      webapp: "https://grocery-app-self.vercel.app",
+    },
+    {
+      id: 2,
+      title: "BlogStudio-AI",
+      description:
+        "An intelligent blogging platform that leverages AI to help users write, optimize, and publish blogs effortlessly. Built with the MERN stack and integrated AI features for smart content generation, SEO insights, and user analytics.",
+
+        image: blog,
+      tags: ["JavaScript", "React JS","Node JS", "Express JS", "MongoDb","JWT", "Rest API","Tailwind CSS","Motion"],
+      github: "https://github.com/MdAjharulIslam/BlogStudio-AI",
+      webapp: "https://blogstudio-ai-m2p9.onrender.com",
+    },
+    {
+      id: 3,
+      title: "Real-Time Chat App",
+      description:
+        "Built a feature-rich real-time chat application with WebSocket technology for instant messaging. The platform supports user authentication, profile management, image sharing, and an intuitive UI for seamless communication.",
+
+        image: chat,
+      tags: ["JavaScript", "React JS","Node JS", "Express JS", "MongoDb","JWT", "Rest API","Tailwind CSS","Motion","Daisy UI", "Socket.io"],
+      github: "https://github.com/MdAjharulIslam/Real-Time-Chat-App",
+      webapp: "https://real-time-chat-app-3jvq.onrender.com",
+    },
+    {
+      id: 4,
+      title: "Netflix 2.0",
+      description:
+        "Developed a fully responsive and dynamic Netflix clone using the MERN stack. Features include user authentication, movie browsing via TMDB API, personalized watchlists, and a seamless streaming experience.",
+
+        image: netflix,
+      tags: ["JavaScript", "React JS","Node JS", "Express JS", "MongoDb","JWT", "Rest API","Tailwind CSS","Motion" ,"Movie API"],
+      github: "https://github.com/MdAjharulIslam/Netflix2.o",
+      webapp: "https://netflix2-o.onrender.com",
+    },
+    {
+      id: 5,
+      title: "E-Commerce App",
+      description:
+       "Built a modern and fully functional e-commerce application with secure authentication, product listings, a shopping cart, and payment integration. Designed for a smooth and seamless shopping experience.",
+
+        image: ecom,
+      tags: ["JavaScript", "React JS","Node JS", "Express JS", "MongoDb","JWT", "Rest API","Tailwind CSS"],
+      github: "https://github.com/MdAjharulIslam/Ecommerce-App",
+      webapp: "https://ecommerce-app-kappa-gilt.vercel.app ",
+    },
+    {
+      id: 6,
+      title: "Real-Time Location Tracker",
+      description:
+        "A web app that tracks and displays users' live locations on a map using Socket.io and Leaflet.js. Features real-time updates, user count display, street/satellite view toggle, and dynamic marker management. Built with Node.js, Express, and the Geolocation API.",
+
+        image: location,
+      tags: [ "Node.js", "MongoDB", "Express","Socket.io"],
+      github: "https://github.com/MdAjharulIslam/REALTIME_TRACkER",
+      webapp: " ",
+    },
+    {
+      id: 7,
+      title: "Journalism-X",
+      description:
+        "Developed a responsive and feature-rich journalism website tailored to provide a seamless news consumption experience. The platform includes dynamic news categories, real-time updates, and an intuitive content management system (CMS) for efficient article publishing.",
+
+        image: journalism,
+      tags: [ "API", "Search Feature", "CSS", "Javascript"],
+      github: "https://github.com/MdAjharulIslam/JournalismX-js",
+      webapp: " ",
+    },
+    {
+      id: 8,
+      title: "Amazon Clone",
+      description:
+        "An Amazon clone built with HTML, CSS, and JavaScript, featuring user authentication, product listings, shopping cart functionality, and a basic checkout system. The project mimics the layout and functionality of Amazon's online store.",
+
+        image: amazon,
+      tags: [ "HTML", "CSS", "Javascript"],
+      github: "https://github.com/MdAjharulIslam/Amazon",
+      webapp: " ",
+    },
+     {
+      id: 9,
+      title: "Coffee Shop App",
+      description:
+        "An e-commerce platform for a coffee shop featuring a product catalog, online order placement, secure payment integration, and customer reviews. Customers can browse the menu, place their orders, and pay online effortlessly.",
+
+        image: cofee,
+      tags: [ "HTML", "CSS", "Javascript"],
+      github: "https://github.com/MdAjharulIslam/coffeeshop-website",
+      webapp: " https://mdajharulislam.github.io/coffeeshop-website/",
+    },
+  ];  
