5e348ab4e4cd27757bc7398a895983b888689dfe
ajharul <mdajharulislam327@gmail.com>
2025-12-18 15:29:19 +0600

    update resume

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -48,7 +48,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/16qHaTme2ZXdQ_5xFV9KV8coq1F8B0uJu/view?usp=drive_link"
+            href="https://drive.google.com/file/d/1YhhhAu4Hv8RZh3d4XkAu6aWsH6SzByl0/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
